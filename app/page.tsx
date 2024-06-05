3c82735be8586c3e840244fd82739487afc0b136
Snayder Rodrigues <snayderrodrigues@gmail.com>
2024-06-04 23:56:53 -0300

    add: main and sidebar components

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
-
+import Sidebar from "@/components/Sidebar";
+import Main from "../components/Main";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center relative bg-black-100 overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-w-7xl w-full">
-        <h1>To-Do App</h1>
+    <main className="flex items-center justify-center relative bg-black-100 overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="flex items-center justify-between max-w-7xl w-full">
+        <Sidebar />
+        <Main />
       </div>
     </main>
   );
